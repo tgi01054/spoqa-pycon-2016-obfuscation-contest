876e28b1a4b14435ce1e0df95e7f4358a56a538e
Won-Sik Kim <tgi01054@gmail.com>
2016-08-14 21:16:01 +0900

    Replace bc command to python inline command

--- calc.py
+++ calc.py
@@ -1,2 +1,32 @@
 import sys
-print(eval(sys.argv[1]))
+import os
+import shlex, subprocess
+from subprocess import call
+from subprocess import PIPE
+
+expr = sys.argv[1]
+
+#if( expr.startswith("+") == True ):
+#        expr = expr[1:]
+#expr = "scale=4; %s" % (expr)	
+#
+#p1 = subprocess.Popen(['echo', expr ],
+#                         stdout=subprocess.PIPE)
+#p2 = subprocess.Popen(['bc'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
+#
+#stdout, stderr = p2.communicate()
+#stdout=stdout.decode()
+#stderr=stderr.decode()
+
+code = "print(%s)" % (expr)
+
+p = subprocess.Popen(['python','-c',code], stdout=PIPE, stderr=PIPE)
+
+stdout, stderr = p.communicate()
+stdout=stdout.decode()
+stderr=stderr.decode()
+
+if( not stderr ):
+	print(stdout)
+	sys.exit(0)	
+sys.exit(-1)	
